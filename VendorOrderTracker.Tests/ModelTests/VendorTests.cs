adad9010461830f6d5ef4b11a5e0d4cbf05f8ed0
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-22 10:30:34 -0600

    properties test fails due to not clearing Vendors._instances

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -11,5 +11,15 @@ namespace VendorOrderTracker.Tests
       Vendor newVendor = new Vendor("test", "test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void VendorConstructor_CreatesVendorWithCorrectProperties_StringStringInt()
+    {
+      string name = "Cathy's Cafe";
+      string des = "Cathy always pays on time";
+      Vendor newVendor = new Vendor(name, des);
+      Assert.AreEqual(name, newVendor.Name);
+      Assert.AreEqual(des, newVendor.Description);
+      Assert.AreEqual(1, newVendor.Id);
+    }
   }
 }

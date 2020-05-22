36560e17318b79e855044e54d67e52a2e2fb72b7
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-22 10:25:15 -0600

    add test and constructor for Vendor

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,1 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_InstanceOfTypeVendorCreated_Vendor()
+    {
+      Vendor newVendor = new Vendor("test", "test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    //public List<Order> Orders = new List<Order> { };
+    public int Id { get; }
+    private static int _currentId = 0;
+    private static List<Vendor> _instances = new List<Vendor> { };
+
+    public Vendor(string name)
+    {
+      Name = name;
+      _currentId++;
+      Id = _currentId;
+      _instances.Add(this);
+    }
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _currentId++;
+      Id = _currentId;
+      _instances.Add(this);
+    }
+  }
+}
